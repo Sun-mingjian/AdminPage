d58bb547e93f403dc2457c8099c664256077262c
Mingjian_Sun <pythonsun1234@gmail.com>
2017-11-18 22:37:24 +1100

    create controller view and compose components

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -46312,19 +46312,8 @@ module.exports = About;
 var React = require('react');
 var AuthorApi = require('../../api/authorApi');
 
-var Authors = React.createClass({displayName: "Authors",
-    getInitialState: function(){
-        return {
-            authors: []
-        };
-    },
-
-    componentWillMount: function(){
-        this.setState({authors: AuthorApi.getAllAuthors() });
-    },
-
-    render: function(){
-        
+var AuthorList = React.createClass({displayName: "AuthorList",
+    render: function(){   
         var createAuthorRow = function(author){
             return (
                 React.createElement("tr", {key: author.id}, 
@@ -46340,14 +46329,13 @@ var Authors = React.createClass({displayName: "Authors",
 
         return (
             React.createElement("div", null, 
-                React.createElement("h1", null, "Authors"), 
                 React.createElement("table", {className: "table"}, 
                     React.createElement("thead", null, 
                         React.createElement("th", null, "ID"), 
                         React.createElement("th", null, "Name")
                     ), 
                     React.createElement("tbody", null, 
-                        this.state.authors.map(createAuthorRow, this)
+                        this.props.authors.map(createAuthorRow, this)
                     )
                 )
             )
@@ -46355,9 +46343,41 @@ var Authors = React.createClass({displayName: "Authors",
     }
 });
 
-module.exports = Authors;
+module.exports = AuthorList;
 
 },{"../../api/authorApi":159,"react":158}],163:[function(require,module,exports){
+"use strict";
+
+var React = require('react');
+var AuthorApi = require('../../api/authorApi');
+var AuthorList = require('./authorList');
+
+var Authors = React.createClass({displayName: "Authors",
+    getInitialState: function(){
+        return {
+            authors: []
+        };
+    },
+
+    componentDidMount: function(){
+        if(this.isMounted()){
+            this.setState({authors: AuthorApi.getAllAuthors() });
+        }
+    },
+
+    render: function(){
+        return (
+            React.createElement("div", null, 
+                React.createElement("h1", null, "Authors"), 
+                React.createElement(AuthorList, {authors: this.state.authors})
+            )
+        );
+    }
+});
+
+module.exports = Authors;
+
+},{"../../api/authorApi":159,"./authorList":162,"react":158}],164:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -46383,7 +46403,7 @@ var Header = React.createClass({displayName: "Header",
 
 module.exports = Header;
 
-},{"react":158}],164:[function(require,module,exports){
+},{"react":158}],165:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -46402,7 +46422,7 @@ var Home = React.createClass({displayName: "Home",
 
 module.exports = Home;
 
-},{"react":158}],165:[function(require,module,exports){
+},{"react":158}],166:[function(require,module,exports){
 $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homePage');
@@ -46449,4 +46469,4 @@ var Authors = require('./components/authors/authorPage');
     render();
 })(window);
 
-},{"./components/about/aboutPage":161,"./components/authors/authorPage":162,"./components/common/header":163,"./components/homePage":164,"jquery":1,"react":158}]},{},[165]);
+},{"./components/about/aboutPage":161,"./components/authors/authorPage":163,"./components/common/header":164,"./components/homePage":165,"jquery":1,"react":158}]},{},[166]);
